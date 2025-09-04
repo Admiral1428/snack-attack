a25bc22f10ef837902c87e0720526794b259dbe8
Tim <tradmiral14@gmail.com>
2025-09-04 11:19:04 -0400

    Implemented sprite speed instead of sleep delay (#13)

--- src/asset/sprite.py
+++ src/asset/sprite.py
@@ -7,46 +7,57 @@ from rect.utils import define_rect
 # Class to define a sprite and its methods
 class Sprite:
     def __init__(
-        self, name, image, center_position, health, can_rotate, hitbox_width, path_width
+        self, name, image, center_position, speed, can_rotate, hitbox_width, path_width
     ):
         self.name = name
         self.image = image
         self.center_position = center_position
-        self.health = health
+        self.speed = speed  # pixels per second
         self.can_rotate = can_rotate
-
         self.hitbox_rect = define_rect(center_position, hitbox_width)
         self.path_rect = define_rect(center_position, path_width)
+
         self.rotation_angle = 0  # + counterclockwise
         self.mirror = False
         self.destroyed = False
+        self.center_pos_decimal = center_position
         self.direction = (0, 0)
         self.desired_direction = ()
         self.motion_vector = (0, 0)
-        self.motion_vector_history = deque(maxlen = 1)
+        self.motion_vector_history = deque(maxlen=1)
         self.animation_start_time = None
         self.animation_sequence = []
 
-    # Method to move the asset
-    def move(self, delta_x, delta_y, maze_grid):
+    # Method to perform shifting to desired location
+    def shift(self, delta_x, delta_y):
+        self.hitbox_rect.move_ip(delta_x, delta_y)
+        self.path_rect.move_ip(delta_x, delta_y)
+        self.center_position = (
+            self.center_position[0] + delta_x,
+            self.center_position[1] + delta_y,
+        )
+        # Set the partial position equal to the new position
+        self.center_pos_decimal = (self.center_position[0], self.center_position[1])
+
+    # Method to move the asset with associated motion vector and orientation operations
+    def move(self, delta_x, delta_y):
         if delta_x != 0 or delta_y != 0:
             # Initialize flag, only to be used if motion vector changes
             orientation_check = False
             # Store current motion vector if new one is different
-            if not self.motion_vector_history or self.motion_vector != (delta_x, delta_y):
+            if not self.motion_vector_history or self.motion_vector != (
+                delta_x,
+                delta_y,
+            ):
                 self.motion_vector_history.append(self.motion_vector)
                 orientation_check = True
-            # Move Rect objects and position
-            self.hitbox_rect.move_ip(delta_x, delta_y)
-            self.path_rect.move_ip(delta_x, delta_y)
-            self.center_position = (
-                self.center_position[0] + delta_x,
-                self.center_position[1] + delta_y,
-            )
+            # Shift Rect objects and position
+            self.shift(delta_x, delta_y)
             # Set new motion vector
             self.motion_vector = (delta_x, delta_y)
             # Determine new orientation if motion vector changed
-            if orientation_check: self.set_orientation()
+            if orientation_check:
+                self.set_orientation()
 
     # Method to check if the asset can move within maze
     # Note that pygame.Rect not inclusive of bottom or right
@@ -69,21 +80,27 @@ class Sprite:
                     return False
         return True
 
-    # Determine and perform move based on direction and move check
-    def perform_move(self, maze_grid):
+    # Determine and perform move based on direction, move check, time, and speed
+    def perform_move(self, maze_grid, game_dt):
         # If nonzero direction was commanded and is possible, move
-        if self.desired_direction and self.can_move(self.desired_direction[0], self.desired_direction[1], maze_grid):
+        if self.desired_direction and self.can_move(
+            self.desired_direction[0], self.desired_direction[1], maze_grid
+        ):
             # Don't override current direction if commanded was (0, 0),
-            # that way previous direction can be attempted if the next 
+            # that way previous direction can be attempted if the next
             # commanded direction is not possible
             if self.desired_direction == (0, 0):
-                self.move(0, 0, maze_grid)
+                self.move(0, 0)
             else:
                 self.direction = (self.desired_direction[0], self.desired_direction[1])
-                self.move(self.direction[0], self.direction[1], maze_grid)
+                # Perform move if the new position rounds to the nearest pixel
+                if self.shift_threshold_met(game_dt):
+                    self.move(self.direction[0], self.direction[1])
         # If current direction is possible, keep moving that way
         elif self.can_move(self.direction[0], self.direction[1], maze_grid):
-            self.move(self.direction[0], self.direction[1], maze_grid)
+            # Perform move if the new position rounds to the nearest pixel
+            if self.shift_threshold_met(game_dt):
+                self.move(self.direction[0], self.direction[1])
         else:
             # No movement possible, so update motion vector with 0, 0
             self.motion_vector = (0, 0)
@@ -180,9 +197,21 @@ class Sprite:
     def collide_check(self, second_sprite: "Sprite"):
         return self.hitbox_rect.colliderect(second_sprite.hitbox_rect)
 
-    # Set sprite health to input value
-    def set_health(self, new_health):
-        self.health = new_health
+    # Check if the fractional shift performed warrants shifting the block by a pixel
+    def shift_threshold_met(self, game_dt):
+        delta_dist = self.speed * game_dt
+        self.center_pos_decimal = (
+            self.center_pos_decimal[0] + delta_dist * self.direction[0],
+            self.center_pos_decimal[1] + delta_dist * self.direction[1],
+        )
+        return (
+            abs(self.center_pos_decimal[0] - self.center_position[0]) > 0.5
+            or abs(self.center_pos_decimal[1] - self.center_position[1]) > 0.5
+        )
+
+    # Set sprite speed to input value
+    def set_speed(self, new_speed):
+        self.speed = new_speed
 
     # Set flag to destroy asset
     def destroy(self, destroy_images):
@@ -199,11 +228,11 @@ class Sprite:
     # Return rect containing hit box
     def get_hit_rect(self):
         return self.hitbox_rect
-    
+
     # Return center position coordinates
     def get_center_position(self):
         return self.center_position
-    
+
     # Return whether animation is occuring
     def is_animating(self):
         return bool(self.animation_start_time)
