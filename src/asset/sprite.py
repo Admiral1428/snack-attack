ca7bd97b0ea53556305c3f2298fa39e3e5c8f1d4
Tim <tradmiral14@gmail.com>
2025-09-07 12:09:20 -0400

    Added new control schemes and small bug fixes (#18)

--- src/asset/sprite.py
+++ src/asset/sprite.py
@@ -53,7 +53,7 @@ class Sprite:
             self.center_position[1] + delta_y,
         )
 
-    # Method to move the asset with associated motion vector and orientation operations
+    # Method to move the sprite with associated motion vector and orientation operations
     def move(self, delta_x, delta_y):
         if delta_x != 0 or delta_y != 0:
             # Initialize flag, only to be used if motion vector changes
@@ -71,15 +71,21 @@ class Sprite:
             # Shift Rect objects and position
             self.shift(delta_x, delta_y)
             # Set new motion vector
-            self.motion_vector = (delta_signs[0], delta_signs[1])
+            self.set_motion_vector(delta_signs[0], delta_signs[1])
             # Determine new orientation if motion vector changed
             if self.can_rotate and orientation_check:
                 self.set_orientation()
 
-    # Method to check if the asset can move within maze
-    # Note that pygame.Rect not inclusive of bottom or right
+    # Method to check if the sprite can move within maze
+    # based on a given delta position
     def can_move(self, delta_x, delta_y, maze_grid):
         temp_rect = self.path_rect.move(delta_x, delta_y)
+        return Sprite.is_path_clear(temp_rect, maze_grid)
+
+    # Method to check if a given rect of interest is within path
+    # Note that pygame.Rect not inclusive of bottom or right
+    @staticmethod
+    def is_path_clear(temp_rect, maze_grid):
         topleft = temp_rect.topleft
         botright = temp_rect.bottomright
         # Check if within bounds of maze
@@ -96,6 +102,60 @@ class Sprite:
                 if maze_grid[row][col] == 1:
                     return False
         return True
+
+    # Method to check if the sprite can move within maze
+    # in the direction of a second sprite, and that a clear
+    # path exists between the two. If true, returns the direction
+    # to be traversed by first sprite.
+    def can_move_towards(self, second_sprite: "Sprite", maze_grid):
+        aligned_horz = False
+        aligned_vert = False
+        # Check if aligned horizontally with second sprite (same y)
+        if self.center_position[1] == second_sprite.center_position[1]:
+            # Move towards second sprite either right or left
+            aligned_horz = True
+            if self.center_position[0] < second_sprite.center_position[0]:
+                move_direction = (1, 0)
+            elif self.center_position[0] > second_sprite.center_position[0]:
+                move_direction = (-1, 0)
+        # Otherwise check if aligned vertically with second sprite (same x)
+        elif self.center_position[0] == second_sprite.center_position[0]:
+            # Move towards second sprite either up or down
+            aligned_vert = True
+            if self.center_position[1] > second_sprite.center_position[1]:
+                move_direction = (0, -1)
+            elif self.center_position[1] < second_sprite.center_position[1]:
+                move_direction = (0, 1)
+
+        # Form a horizontal rect between the two for checking
+        if aligned_horz:
+            temp_topleft_x = min(
+                self.path_rect.topleft[0], second_sprite.path_rect.topleft[0]
+            ) + int(self.path_width / 2)
+            temp_topleft_y = self.path_rect.topleft[1]
+            temp_width = abs(
+                self.path_rect.center[0] - second_sprite.path_rect.center[0]
+            )
+            temp_height = self.path_rect.height
+        # Form a vertical rect between the two for checking
+        elif aligned_vert:
+            temp_topleft_x = self.path_rect.topleft[0]
+            temp_topleft_y = min(
+                self.path_rect.topleft[1], second_sprite.path_rect.topleft[1]
+            ) + int(self.path_width / 2)
+            temp_width = self.path_rect.width
+            temp_height = abs(
+                self.path_rect.center[1] - second_sprite.path_rect.center[1]
+            )
+
+        if not (aligned_horz or aligned_vert):
+            return ()
+
+        # Check if temp rect resides within a clear space within maze path
+        temp_rect = pygame.Rect(temp_topleft_x, temp_topleft_y, temp_width, temp_height)
+        if Sprite.is_path_clear(temp_rect, maze_grid):
+            return move_direction
+        return ()
 
     # Determine and perform move based on direction, move check, time, and speed
     def perform_move(self, maze_grid, game_dt):
@@ -120,7 +180,7 @@ class Sprite:
             if self.desired_direction == (0, 0):
                 self.move(0, 0)
             else:
-                self.direction = (self.desired_direction[0], self.desired_direction[1])
+                self.set_direction(self.desired_direction[0], self.desired_direction[1])
                 if do_move:
                     # Move delta_dist, or max possible if that is too far
                     for i in range(delta_dist, 0, -1):
@@ -144,7 +204,7 @@ class Sprite:
             self.move(max_dist * self.direction[0], max_dist * self.direction[1])
         else:
             # No movement possible, so update motion vector with 0, 0
-            self.motion_vector = (0, 0)
+            self.set_motion_vector(0, 0)
 
     # Define orientation based on current and previous direction
     def set_orientation(self):
@@ -223,9 +283,13 @@ class Sprite:
                     self.image = frame.get("image")
                     self.animation_index = index
 
-    # Set sprite commanded direction to input values
-    def set_direction(self, x_direction, y_direction):
+    # Set sprite desired direction to input values
+    def set_desired_direction(self, x_direction, y_direction):
         self.desired_direction = (x_direction, y_direction)
+
+    # Set sprite direction to input values
+    def set_direction(self, x_direction, y_direction):
+        self.direction = (x_direction, y_direction)
 
     # Move towards player if direction possible,
     # otherwise random navigation, only backtracking if needed
@@ -235,34 +299,28 @@ class Sprite:
             # Possible directions: Right, Left, Up, Down
             dirs = [(1, 0), (-1, 0), (0, -1), (0, 1)]
 
+            # Check if possible to move towards player with clear path
             aligned_second_sprite = False
-            # If aligned vertically with second sprite
-            if self.center_position[1] == second_sprite.center_position[1]:
-                # Move towards second sprite either right or left
+            move_direction = self.can_move_towards(second_sprite, maze_grid)
+            if move_direction:
                 aligned_second_sprite = True
-                if self.path_rect.right < second_sprite.path_rect.left:
-                    self.set_direction(dirs[0][0], dirs[0][1])
-                elif self.path_rect.left > second_sprite.path_rect.right:
-                    self.set_direction(dirs[1][0], dirs[1][1])
-
-            # If aligned horizontally with second sprite
-            elif self.center_position[0] == second_sprite.center_position[0]:
-                # Move towards second sprite either up or down
-                aligned_second_sprite = True
-                if self.path_rect.top > second_sprite.path_rect.bottom:
-                    self.set_direction(dirs[2][0], dirs[2][1])
-                elif self.path_rect.bottom < second_sprite.path_rect.top:
-                    self.set_direction(dirs[3][0], dirs[3][1])
+                self.set_desired_direction(move_direction[0], move_direction[1])
 
             # If current direction is 0, 0, choose a random direction
             if self.desired_direction == (0, 0) or not self.desired_direction:
                 rand_direction = dirs[random.randint(0, 3)]
-                self.set_direction(rand_direction[0], rand_direction[1])
+                self.set_desired_direction(rand_direction[0], rand_direction[1])
 
             # Check current direction, then orthogonal directions, then reverse
-            reverse_direction = (-1 * self.desired_direction[0], -1 * self.desired_direction[1])
+            reverse_direction = (
+                -1 * self.desired_direction[0],
+                -1 * self.desired_direction[1],
+            )
             orthog_direction_1 = (self.desired_direction[1], self.desired_direction[0])
-            orthog_direction_2 = (-1 * self.desired_direction[1], -1 * self.desired_direction[0])
+            orthog_direction_2 = (
+                -1 * self.desired_direction[1],
+                -1 * self.desired_direction[0],
+            )
 
             directions_to_check = [
                 self.desired_direction,
@@ -272,24 +330,24 @@ class Sprite:
             ]
             valid_indices = []
             for index, direction in enumerate(directions_to_check):
-                if self.can_move(
-                    direction[0], direction[1], maze_grid
-                ):
+                if self.can_move(direction[0], direction[1], maze_grid):
                     valid_indices.append(index)
 
             # If no directions are possible or only reverse possible, these take precedent.
-            # Otherwise, pick a random direction of the remaining possibilities if not 
+            # Otherwise, pick a random direction of the remaining possibilities if not
             # aligned with the second sprite (go in player direction if it's possible).
             if not valid_indices:
-                self.set_direction(0, 0)
+                self.set_desired_direction(0, 0)
             elif valid_indices == [3]:
-                self.set_direction(directions_to_check[3][0], directions_to_check[3][1])
+                self.set_desired_direction(
+                    directions_to_check[3][0], directions_to_check[3][1]
+                )
             elif aligned_second_sprite and 0 in valid_indices:
                 pass
             else:
                 non_reverse_dirs = [i for i in valid_indices if i != 3]
                 chosen_index = random.choice(non_reverse_dirs)
-                self.set_direction(
+                self.set_desired_direction(
                     directions_to_check[chosen_index][0],
                     directions_to_check[chosen_index][1],
                 )
@@ -309,7 +367,7 @@ class Sprite:
         self.animation_index = 0
         self.animation_start_time = None
 
-    # Set flag to destroy asset
+    # Set flag to destroy sprite
     def toggle_destroy(self):
         self.destroyed = not self.destroyed
 
@@ -341,6 +399,6 @@ class Sprite:
     def is_animating(self):
         return bool(self.animation_start_time)
 
-    # Return motion vector
-    def get_motion_vector(self):
-        return self.motion_vector
+    # Set motion vector
+    def set_motion_vector(self, x_direction, y_direction):
+        self.motion_vector = (x_direction, y_direction)
