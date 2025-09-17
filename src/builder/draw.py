1624ef0aed7d0779a810b75eff684b7306e2888d
Tim Roble <tradmiral14@gmail.com>
2025-09-17 15:21:02 -0400

    Added supporting functions for level builder refactoring

---
+++ src/builder/draw.py
@@ -0,0 +1,246 @@
+import os
+import pygame
+import time
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from settings import config as cfg
+from rect.utils import define_rect, shift_rect_to_divisible_pos
+from rect.draw import draw_square, draw_asset, draw_maze
+from grid.utils import invert_maze_to_grid, grid_space
+from fileio.load import import_image_dir
+from fileio.export import (
+    export_path_coords_to_csv,
+    export_asset_coords_to_csv,
+    export_metadata,
+    move_files,
+)
+from path.utils import (
+    rect_within_boundary,
+    rect_gives_uniform_path,
+)
+
+
+# Function to process inputs for path drawing
+def process_path_inputs(
+    flags,
+    chosen_coords,
+    image_boundary,
+    block_width,
+    min_block_spacing,
+    current_arrow,
+    arrow_index,
+):
+    if flags.arrow_pressed:
+        if not chosen_coords:
+            # If no previous points chosen with mouse, start at upper left corner
+            current_selected_pos = (
+                cfg.DRAW_IMAGE_X + image_boundary + int(block_width / 2),
+                cfg.DRAW_IMAGE_Y + image_boundary + int(block_width / 2),
+            )
+        else:
+            try:
+                current_selected_pos = chosen_coords[-1]
+                if current_arrow == pygame.K_UP:
+                    current_selected_pos = (
+                        chosen_coords[arrow_index][0],
+                        chosen_coords[arrow_index][1] - min_block_spacing,
+                    )
+                elif current_arrow == pygame.K_DOWN:
+                    current_selected_pos = (
+                        chosen_coords[arrow_index][0],
+                        chosen_coords[arrow_index][1] + min_block_spacing,
+                    )
+                elif current_arrow == pygame.K_LEFT:
+                    current_selected_pos = (
+                        chosen_coords[arrow_index][0] - min_block_spacing,
+                        chosen_coords[arrow_index][1],
+                    )
+                elif current_arrow == pygame.K_RIGHT:
+                    current_selected_pos = (
+                        chosen_coords[arrow_index][0] + min_block_spacing,
+                        chosen_coords[arrow_index][1],
+                    )
+            except:
+                # Catching case where index may be out of bounds
+                pass
+    else:
+        current_selected_pos = pygame.mouse.get_pos()
+
+    return current_selected_pos
+
+
+# Function to draw path
+def draw_path(
+    flags,
+    chosen_coords,
+    maze_height,
+    maze_width,
+    image_boundary,
+    block_width,
+    min_block_spacing,
+    current_arrow,
+    screen,
+    dirty_rects,
+    shifted_coords_history,
+    arrow_index,
+):
+
+    current_selected_pos = process_path_inputs(
+        flags,
+        chosen_coords,
+        image_boundary,
+        block_width,
+        min_block_spacing,
+        current_arrow,
+        arrow_index,
+    )
+
+    my_rect = define_rect(current_selected_pos, block_width)
+    my_rect = shift_rect_to_divisible_pos(
+        my_rect,
+        cfg.DRAW_IMAGE_X,
+        cfg.DRAW_IMAGE_Y,
+        min_block_spacing,
+        image_boundary,
+    )
+
+    # If chosen coordinate from arrow keys is already in chosen coordinates,
+    # save that space so that user can backtrack.
+    # Subsequently highlight the block with flashing so that they know where
+    # they are for context.
+    if flags.arrow_pressed and my_rect.center in chosen_coords:
+        arrow_index = chosen_coords.index(my_rect.center)
+        for i in range(3):
+            draw_square(my_rect, screen, cfg.COLORS["white"], dirty_rects)
+            time.sleep(1 / 30)
+            draw_square(my_rect, screen, cfg.COLORS["black"], dirty_rects)
+            time.sleep(1 / 30)
+    else:
+        arrow_index = -1
+
+    # Reset flag for arrow key press
+    flags.arrow_pressed = False
+
+    # Coarsen inputs prior to checking maze grid to optimize runtime
+    coords_scaled = []
+    for coord in chosen_coords:
+        coords_scaled.append(
+            (
+                int((coord[0] - cfg.DRAW_IMAGE_X - image_boundary) / cfg.SCALE_FACTOR),
+                int((coord[1] - cfg.DRAW_IMAGE_Y - image_boundary) / cfg.SCALE_FACTOR),
+            )
+        )
+    new_center = (
+        int((my_rect.center[0] - cfg.DRAW_IMAGE_X - image_boundary) / cfg.SCALE_FACTOR),
+        int((my_rect.center[1] - cfg.DRAW_IMAGE_Y - image_boundary) / cfg.SCALE_FACTOR),
+    )
+    my_rect_scaled = define_rect(new_center, cfg.BLOCK_WIDTH)
+
+    # Check to make sure the current shifted position is not the same as
+    # the last, and that it's not in chosen coords. If all criteria met,
+    # draw the new path square
+    if (
+        not shifted_coords_history or my_rect.center != shifted_coords_history[-1]
+    ) and my_rect.center not in chosen_coords:
+        shifted_coords_history.append(my_rect.center)
+
+        if rect_within_boundary(
+            my_rect,
+            cfg.DRAW_IMAGE_X,
+            cfg.DRAW_IMAGE_Y,
+            image_boundary,
+            maze_width,
+            maze_height,
+        ) and rect_gives_uniform_path(
+            coords_scaled,
+            my_rect_scaled,
+            cfg.MAZE_WIDTH,
+            cfg.MAZE_HEIGHT,
+            0,
+            0,
+            0,
+            cfg.BLOCK_WIDTH,
+        ):
+            # Add updated mouse position to list of previous points
+            chosen_coords.append((my_rect.center))
+            draw_square(my_rect, screen, cfg.COLORS["black"], dirty_rects)
+
+    return flags, arrow_index
+
+
+# Function to erase a path rect, then re-drawing overlapping squares
+def erase_and_redraw(
+    cur_coord,
+    block_width,
+    maze_colors,
+    maze_color_index,
+    screen,
+    dirty_rects,
+    chosen_coords,
+):
+    # Draw a block overtop the old coordinate
+    my_rect = define_rect(cur_coord, block_width)
+    selected_color = maze_colors[maze_color_index]
+    draw_square(my_rect, screen, selected_color, dirty_rects)
+
+    # Draw previous coordinates which were overlapping erased square
+    for coord in chosen_coords:
+        temp_rect = define_rect(coord, block_width)
+        if temp_rect.colliderect(my_rect):
+            draw_square(temp_rect, screen, cfg.COLORS["black"], dirty_rects)
+
+
+# Function to undo drawing of path rect
+def undo_path_rect(
+    chosen_coords,
+    shifted_coords_history,
+    block_width,
+    maze_colors,
+    maze_color_index,
+    screen,
+    dirty_rects,
+    flags,
+):
+    # Remove last coordinate (undo)
+    last_coord = chosen_coords.pop()
+
+    # Remove last shifted coordinate from history
+    shifted_coords_history.pop()
+
+    erase_and_redraw(
+        last_coord,
+        block_width,
+        maze_colors,
+        maze_color_index,
+        screen,
+        dirty_rects,
+        chosen_coords,
+    )
+
+    # Reset flag for right click
+    flags.mouse_right_click = False
+
+    return flags
+
+
+# Function to undo last asset placement
+def undo_asset_placement(
+    asset_coords, asset_defs, block_width, screen, dirty_rects, flags
+):
+    # Get last coordinate
+    remove_asset_coord = asset_coords.pop()
+
+    # Set asset coordinate in dictionary to ()
+    for asset in asset_defs:
+        if asset.get("location") == remove_asset_coord:
+            asset["location"] = ()
+            break
+
+    # Erase asset on maze by drawing black square
+    temp_rect = define_rect(remove_asset_coord, block_width)
+    draw_square(temp_rect, screen, cfg.COLORS["black"], dirty_rects)
+
+    # Reset flag for right click
+    flags.mouse_right_click = False
+
+    return flags
---
+++ src/builder/input.py
@@ -0,0 +1,137 @@
+import pygame
+from builder.action import (
+    export_maze_files,
+    change_maze_color,
+    cycle_level_speed,
+    cycle_enemy_quantity,
+    init_asset_placement,
+    assign_asset_loc,
+)
+
+
+# Function to handle inputs for level builder
+def process_input(
+    event,
+    flags,
+    screen,
+    fonts,
+    asset_coords,
+    maze_width,
+    maze_height,
+    min_block_spacing,
+    block_width,
+    image_boundary,
+    maze_colors,
+    chosen_coords,
+    dirty_rects,
+    enemy_quantity,
+    enemy_index,
+    level_speed_index,
+    maze_color_index,
+    asset_letters,
+    asset_defs,
+):
+    current_arrow = None
+    flags.arrow_pressed = False
+    if event.type == pygame.QUIT:
+        flags.running = False
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:  # Left mouse button
+            flags.mouse_left_held = True
+        elif event.button == 3:  # Right mouse button
+            flags.mouse_right_click = True
+    elif event.type == pygame.MOUSEBUTTONUP:
+        if event.button == 1:  # Left mouse button
+            flags.mouse_left_held = False
+    elif event.type == pygame.KEYDOWN:
+        if flags.maze_draw and event.key in [
+            pygame.K_UP,
+            pygame.K_DOWN,
+            pygame.K_LEFT,
+            pygame.K_RIGHT,
+        ]:
+            flags.arrow_pressed = True
+            match event.key:
+                case pygame.K_UP:
+                    current_arrow = pygame.K_UP
+                case pygame.K_DOWN:
+                    current_arrow = pygame.K_DOWN
+                case pygame.K_LEFT:
+                    current_arrow = pygame.K_LEFT
+                case pygame.K_RIGHT:
+                    current_arrow = pygame.K_RIGHT
+        elif event.key == pygame.K_c and not flags.maze_draw:
+            export_maze_files(
+                screen,
+                asset_coords,
+                maze_width,
+                maze_height,
+                image_boundary,
+                chosen_coords,
+                maze_colors,
+                maze_color_index,
+                level_speed_index,
+                enemy_quantity,
+                enemy_index,
+                asset_defs,
+            )
+        elif event.key == pygame.K_w and flags.maze_draw:
+            maze_color_index = change_maze_color(
+                screen,
+                maze_color_index,
+                maze_colors,
+                image_boundary,
+                maze_width,
+                maze_height,
+            )
+        elif event.key == pygame.K_f and flags.maze_draw:
+            level_speed_index = cycle_level_speed(
+                level_speed_index, screen, dirty_rects, fonts
+            )
+        elif event.key == pygame.K_j and flags.maze_draw:
+            enemy_index = cycle_enemy_quantity(
+                "corn", enemy_index, enemy_quantity, screen, fonts, dirty_rects
+            )
+        elif event.key == pygame.K_k and flags.maze_draw:
+            enemy_index = cycle_enemy_quantity(
+                "tomato", enemy_index, enemy_quantity, screen, fonts, dirty_rects
+            )
+        elif event.key == pygame.K_l and flags.maze_draw:
+            enemy_index = cycle_enemy_quantity(
+                "pumpkin", enemy_index, enemy_quantity, screen, fonts, dirty_rects
+            )
+        elif event.key == pygame.K_a:
+            asset_letters, asset_defs = init_asset_placement(
+                chosen_coords,
+                image_boundary,
+                maze_width,
+                maze_height,
+                screen,
+                flags,
+                fonts,
+                asset_letters,
+                asset_defs,
+            )
+        elif not flags.maze_draw and event.unicode.lower() in asset_letters:
+            asset_defs = assign_asset_loc(
+                event,
+                asset_coords,
+                chosen_coords,
+                dirty_rects,
+                screen,
+                image_boundary,
+                min_block_spacing,
+                block_width,
+                fonts,
+                asset_defs,
+            )
+
+    return (
+        flags,
+        current_arrow,
+        level_speed_index,
+        enemy_index,
+        maze_color_index,
+        asset_letters,
+        asset_defs,
+    )
